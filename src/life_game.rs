1c4442bd76028bbe26216329f5d652b450511e24
gordonmcvey <grj.mcvey@gmail.com>
2026-01-26 22:56:55 +0000

    Added a display formatter for the Game struct

--- src/life_game.rs
+++ src/life_game.rs
@@ -1,3 +1,6 @@
+use colored::Colorize;
+use std::fmt::Display;
+
 type CellData = Vec<Vec<bool>>;
 
 pub struct Game {
@@ -28,10 +31,34 @@ impl Game {
 
         for row in 0 .. self.height {
             for column in 0 .. self.width {
-                
+                // println!("{} {}", row, column);
             }
         }
 
         self.cells = new_state;
+    }
+}
+
+impl Display for Game {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut output = String::new();
+
+        for row in self.cells.iter() {
+            for cell in row.iter() {
+                let cell_output = {
+                    if *cell {
+                        String::from(" ").on_bright_white()
+                    } else {
+                        String::from(" ").on_bright_black()
+                    }
+                };
+
+                output.push_str(format!("{}", cell_output).as_str());
+            }
+            output.push_str("\n");
+        }
+
+        write!(f, "{}", output)?;
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use life_game::Game;
+use std::thread;
+use std::time::Duration;
+
 mod life_game;
 
 fn main() {
-    print!("\x1B[2J\x1B[1;1H");
-    println!("Hello, world!");
+    let mut game = Game::from_data(
+        vec![
+            vec![true, false, false, true, false, false, true, false, false],
+            vec![true, false, false, true, false, false, true, false, false],
+            vec![false, false, true, false, false, true, false, false, true],
+            vec![false, false, false, false, false, false, false, false, false],
+            vec![false, false, true, false, false, true, false, false, true],
+        ]
+    );
+
+    loop {
+        print!("\x1B[2J\x1B[1;1H");
+        print!("{}", game);
+        thread::sleep(Duration::from_millis(500));
+        game.step();
+    }
 }
