35fdcc38f6d79a6ec897f2e09a1557d1827d3cd5
gordonmcvey <grj.mcvey@gmail.com>
2026-01-28 12:12:52 +0000

    User prompting moved to its own function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use crate::life_game::builder;
 use crate::life_game::CellData;
 use life_game::Game;
 use std::cmp::min;
+use std::num::ParseIntError;
 use std::time::Duration;
 use std::{io, thread};
 
@@ -10,22 +11,8 @@ mod life_game;
 fn main() {
     let (width, height) = space_for_game();
     let mut starting_state: CellData = vec![vec![false; width - 2]; height - 2];
-    let mut input = String::new();
 
-    println!("Select puzzle:");
-    println!("1: Preset starting state");
-    println!("2: A flock of seagulls (gliders)");
-    println!("3: Lightweight spaceship invasion");
-    println!("4: Random population, 5%");
-    println!("5: Random population, 10%");
-    println!("6: Random population, 20%");
-    println!();
-
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-
-    match input.trim().parse() {
+    match prompt() {
         Ok(1) => {
             // gliders
             builder::glider(&mut starting_state, 0, 0);
@@ -65,6 +52,25 @@ fn main() {
     }
 
     println!("Game over!  State stabilised after {} iterations", game.iteration());
+}
+
+fn prompt() -> Result<i32, ParseIntError> {
+    let mut input = String::new();
+
+    println!("Select puzzle:");
+    println!("1: Preset starting state");
+    println!("2: A flock of seagulls (gliders)");
+    println!("3: Lightweight spaceship invasion!");
+    println!("4: Random population, 5%");
+    println!("5: Random population, 10%");
+    println!("6: Random population, 20%");
+    println!();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    input.trim().parse::<i32>()
 }
 
 fn space_for_game() -> (usize, usize) {
