380df036e501247bc6beb033ba175428c59c53b8
gordonmcvey <grj.mcvey@gmail.com>
2026-04-29 10:17:42 +0100

    Implemented a naive multi-threaded solver

--- src/life_game/solver.rs
+++ src/life_game/solver.rs
@@ -1,4 +1,6 @@
 use crate::life_game::{CellData, Game};
+use std::sync::Arc;
+use std::thread;
 
 pub(crate) trait Solver {
     fn compute_state(&self, game: &Game) -> CellData;
@@ -35,6 +37,10 @@ pub(crate) trait Solver {
 
 pub(crate) struct SingleThreadedSolver;
 
+pub(crate) struct ThreadedSolver {
+    thread_count: usize,
+}
+
 impl Solver for SingleThreadedSolver {
     fn compute_state(&self, game: &Game) -> CellData {
         let mut new_state = vec![vec![false; game.dimensions.width]; game.dimensions.height];
@@ -50,5 +56,68 @@ impl Solver for SingleThreadedSolver {
         }
 
         new_state
+    }
+}
+
+impl ThreadedSolver {
+    pub(crate) fn new(thread_count: usize) -> Self {
+        Self { thread_count }
+    }
+}
+
+impl Solver for ThreadedSolver {
+
+    fn compute_state(&self, game: &Game) -> CellData {
+        let game_ref = Arc::new(game);
+        let current = Arc::new(game_ref.game_state.clone());
+
+        let handles: Vec<_> = (0..self.thread_count)
+            .map(|thread_id| {
+                // let game_current = Arc::clone(&game_ref);
+                let thread_current = Arc::clone(&current);
+                let width = game.dimensions.width;
+                let height = game.dimensions.height;
+                let chunk_size = height.div_ceil(self.thread_count);
+
+                thread::spawn(move || {
+                    let mut new_chunk: CellData = Vec::new();
+                    let row_start = thread_id * chunk_size;
+                    let row_end = (row_start + chunk_size).min(height);
+                    // println!("Thread {} is alive for rows {} = {}", thread_id, row_start, row_end);
+
+                    for row in row_start..row_end {
+                        let mut new_row = vec![false; width];
+                        for column in 0..width {
+                            let is_alive = thread_current[row][column];
+                            // @todo Refactor get_living_neighbour_count to not require a game reference
+                            // let living_neighbours = Self::get_living_neighbour_count(game_ref, row, column);
+                            let above = (row + height - 1) % height;
+                            let below = (row + 1) % height;
+                            let left = (column + width - 1) % width;
+                            let right = (column + 1) % width;
+
+                            let living_neighbours = (if thread_current[above][left] { 1 } else { 0 }
+                                + if thread_current[above][column] { 1 } else { 0 }
+                                + if thread_current[above][right] { 1 } else { 0 }
+                                + if thread_current[row][left] { 1 } else { 0 }
+                                + if thread_current[row][right] { 1 } else { 0 }
+                                + if thread_current[below][left] { 1 } else { 0 }
+                                + if thread_current[below][column] { 1 } else { 0 }
+                                + if thread_current[below][right] { 1 } else { 0 });
+
+                            new_row[column] = Self::decide_state(is_alive, living_neighbours);
+                        }
+                        new_chunk.push(new_row);
+                    }
+
+                    new_chunk
+                })
+            })
+            .collect();
+
+        handles
+            .into_iter()
+            .flat_map(|h: std::thread::JoinHandle<std::vec::Vec<_>>| h.join().unwrap())
+            .collect()
     }
 }
